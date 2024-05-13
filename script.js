4dde35162c971ed1863d2b672f32c25e3eafbfb0
Nestor Debiesme <nedebies@student.s19.be>
2024-05-13 22:09:40 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
+
 function toggleNightMode() {
     var element = document.body;
     element.classList.toggle("night-mode");
@@ -6,4 +7,23 @@ function toggleNightMode() {
     var button = document.getElementById("night-mode-btn");
     var buttonText = element.classList.contains("night-mode") ? "🌞" : "🌜";
     button.textContent = buttonText;
+}
+
+function toggleSidebar() {
+    var sidebarMenu = document.getElementById("sidebar-menu");
+    var sidebarToggleButton = document.getElementById("sidebar-toggle-btn");
+    if (sidebarMenu.style.left === "-250px") {
+        sidebarMenu.style.left = "0";
+        sidebarToggleButton.textContent = "✕"; // Change button icon
+    } else {
+        sidebarMenu.style.left = "-250px";
+        sidebarToggleButton.textContent = "☰"; // Change button icon
+    }
+}
+
+function hideSidebar() {
+    var sidebarMenu = document.getElementById("sidebar-menu");
+    sidebarMenu.style.left = "-250px";
+    var sidebarToggleButton = document.getElementById("sidebar-toggle-btn");
+    sidebarToggleButton.textContent = "☰"; // Change button icon
 }
