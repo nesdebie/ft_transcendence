9f81e5d024b6e0925895e7e6ab2fabf8a59a1ea8
Nestor Debiesme <nesdebie@fu-r6-p5.s19.be>
2024-05-27 12:28:12 +0200

    AAAAAAAAAA

---
+++ spa/static/spa/js/buttons.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#nightCityModeBtn").click(function(){
+      $("body").toggleClass("night-city-mode");
+    });
+});
---
+++ spa/static/spa/js/router.js
@@ -0,0 +1,49 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+const routes = {
+    404: "../templates/spa/pages/404.html",
+    "/": "../templates/spa/pages/home.html",
+    "/about": "../templates/spa/pages/about.html",
+    "/shifumi": "../templates/spa/pages/shifumi.html",
+    "/pong": "../templates/spa/pages/pong.html",
+    "/logout": "../templates/spa/pages/logout.html",
+};
+
+function initializePong() {
+    board = document.getElementById("pong");
+    if (board) {
+        board.height = boardHeight;
+        board.width = boardWidth;
+        context = board.getContext("2d"); //used for drawing on the board
+
+        //draw initial player1
+        context.fillStyle = "skyblue";
+        context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
+        requestAnimationFrame(update);
+        document.addEventListener("keyup", movePlayer);
+    } else {
+        console.error('Canvas element with id "pong" not found.');
+    }
+}
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+
+    // Initialize Pong game if the current route is /pong
+    if (path === "/pong") {
+        initializePong();
+    }
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
