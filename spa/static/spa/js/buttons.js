e700e2f4e5bc0bedfa2a9edb3e59f5fdcb7bccd5
Maja Kapturska <majakapturska@mbp-de-maja.home>
2024-06-10 18:04:09 +0200

    fix bug redirection login

--- spa/static/spa/js/buttons.js
+++ spa/static/spa/js/buttons.js
@@ -20,23 +20,21 @@ $(document).ready(function(){
     $("#profile-button").click(function(){
         redirectToRoute("/profile");
     });
-updateSidebar();
+    updateSidebar();
 });
 
 document.body.addEventListener('submit', function(event) {
-	
 	if (event.target.id === 'register-form') {
 		event.preventDefault();
 		register(event); }
 	else if (event.target.id === 'login-form'){
 		event.preventDefault();
 		login(event);}
-updateSidebar();
+    updateSidebar();
 });
 
 document.body.addEventListener('click', function(event) {
 	if (event.target.id === 'logout-button') {
 		logout(event);
     }
-updateSidebar();
 });
