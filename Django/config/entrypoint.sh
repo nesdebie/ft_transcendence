9670985ab5327b6fee77ec5c68f12c717fd852f3
Arthur Minjauw <aminjauw@fu-r5-p6.s19.be>
2024-06-12 17:19:15 +0200

    docker compose working perfectly

--- Django/config/entrypoint.sh
+++ Django/config/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+python3 manage.py makemigrations
+
 # Run migrations
 python3 manage.py migrate
 
-# Start the Gunicorn server
-gunicorn --bind 0.0.0.0:8000 SinglePageApp.wsgi:application
+# Start the Gunicorn server "Right way"
+#gunicorn --bind 0.0.0.0:8000 SinglePageApp.wsgi:application
+
+python3 manage.py runserver 0.0.0.0:8000
