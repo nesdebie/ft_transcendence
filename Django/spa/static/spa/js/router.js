24e7557986cd5213978aa1c8da8a5e59338de6a6
Nestor Debiesme <nedebies@student.s19.be>
2024-08-14 23:11:23 +0200

    fix visuel

--- Django/spa/static/spa/js/router.js
+++ Django/spa/static/spa/js/router.js
@@ -54,6 +54,9 @@ function call_page_functions(path) {
     if (path.startsWith('/chat/') && path !== '/chat/') {
         initChat();
     }
+    if (document.body.classList.contains("cyberpunk")) {
+        applyNightCityMode();
+    }
     // if (document.getElementById('user-username'))
     //  fetchUserProfileData();
     //if ...
